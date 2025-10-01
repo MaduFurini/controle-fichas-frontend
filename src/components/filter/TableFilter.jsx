400d4f7f72939929bc193eb082dee257d72bb450
MaduFurini <madufurini@gmail.com>
2025-10-01 01:12:29 -0300

    Fix: Adjust filter and table structure

--- src/components/filter/TableFilter.jsx
+++ src/components/filter/TableFilter.jsx
@@ -1,153 +1,141 @@
-import {useApp} from "../../contexts/AppContext.jsx";
-import {useEffect, useState} from "react";
-import {useSearchParams} from "react-router-dom";
 import {Controller, useForm} from "react-hook-form";
-import {Card, Collapse, Grid, MenuItem, TextField} from "@mui/material";
-import Index from "../datePicker/index.jsx";
-import dayjs from 'dayjs';
+import {useSearchParams} from "react-router-dom";
+import {Card, Collapse, Grid, TextField} from "@mui/material";
+import {useApp} from "../../contexts/AppContext.jsx";
 import {TableFilterButtons} from "./TableFilterButtons.jsx";
+import dayjs from "dayjs";
+import DateRangePicker from "../datePicker/DateRangePicker.jsx";
 
 export function TableFilter({
-                                isOpenFilters,
-                                isLoadingList,
+                                isLoading,
                                 communities,
-                                item,
-                                hasDate
-                            }) {
-    const {setCommunityIdSession} = useApp();
+                                itemName,
+                                hasDate,
+                                hasCommunity,
+}) {
+    const { setCommunityIdSession } = useApp();
 
     const [searchParams, setSearchParams] = useSearchParams();
-    const [isSearched, setIsSearched] = useState(false);
 
-    const [dateRange, setDateRange] = useState([null, null]);
+    const communityId = searchParams.get('communityId');
+    const nome = searchParams.get("nome");
+    const data_inicio = searchParams.get("data_inicio");
+    const data_fim = searchParams.get("data_fim");
 
-    const communityId = searchParams.get("communityId");
-    const name = searchParams.get("name");
-    const initialDate = searchParams.get("initialDate");
-    const finishDate = searchParams.get("finishDate");
+    const perPage = searchParams.get("perPage");
 
-    const perPage = searchParams.get('perPage');
+    const { handleSubmit, control, setValue, watch, reset } = useForm({
+        defaultValues: {
+            communityId: communityId ?? "",
+            nome: nome ?? "",
+            data_inicio: data_inicio ?? "",
+            data_fim: data_fim ?? "",
+        },
+    });
 
-    const {
-        handleSubmit,
-        reset,
-        control,
-        setValue,
-        formState: { errors },
-    } = useForm();
-
-    useEffect(() => {
-        reset({
-            communityId: communityId ?? '',
-            name: name ?? '',
-        })
-    }, [communityId, name, reset]);
+    const dateInicio = watch("data_inicio");
+    const dateFim = watch("data_fim");
 
     function handleSubmitFilter(data) {
         for (const key in data) {
-            if (data[key] === '') {
+            if (data[key] === "") {
                 delete data[key];
             }
-
-            const queryFilters = {
-                ...data,
-                page: '1',
-                perPage: perPage ?? '10',
-            }
-
-            setSearchParams(queryFilters);
-
-            if (data.communityId) setCommunityIdSession(data.communityId);
         }
+
+        const queryFilters = {
+            ...data,
+            page: "1",
+            perPage: perPage ?? "10",
+        };
+
+        setSearchParams(queryFilters);
+
+        if (data.communityId) setCommunityIdSession(data.communityId);
     }
 
     return (
-        <Grid item size={12} component="form" onSubmit={handleSubmit(handleSubmitFilter)}>
-            <Collapse in={isOpenFilters}>
-                <Card variant="outlined" sx={{ padding: 2 }}>
-                    <Grid container spacing={2}>
-                        <Grid item size={6}>
-                            <Controller
-                                name="communityId"
-                                control={control}
-                                defaultValue=""
-                                render={({ field }) => (
-                                    <TextField
-                                        select
-                                        label="Comunidade"
-                                        fullWidth
-                                        size="small"
-                                        {...field}
-                                    >
-                                        {/*{communities.length > 0 && communities.map((option) => (*/}
-                                        {/*    <MenuItem key={option.id} value={option.id}>*/}
-                                        {/*        {option.name}*/}
-                                        {/*    </MenuItem>*/}
-                                        {/*))}*/}
-                                    </TextField>
-                                )}
-                            />
-                        </Grid>
-
-                        <Grid item size={6}>
-                            <Controller
-                                name="name"
-                                control={control}
-                                defaultValue=""
-                                render={({ field }) => (
-                                    <TextField
-                                        label={"Nome " + item}
-                                        fullWidth
-                                        size="small"
-                                        {...field}
-                                    />
-                                )}
-                            />
-                        </Grid>
-
-                        {hasDate && (
-                            <Grid item size={6}>
-                                <Index
-                                    value={dateRange}
-                                    bgColor='#fff'
-                                    onChange={(value) => {
-                                        setDateRange(value);
-                                        const [start, end] = value;
-                                        setValue('data_inicio', start ? dayjs(start).format('YYYY-MM-DD') : '');
-                                        setValue('data_fim', end ? dayjs(end).format('YYYY-MM-DD') : '');
-                                        setIsSearched(false);
-                                    }}
-                                    onReset={() => {
-                                        setDateRange([null, null]);
-                                        setValue('data_inicio', '');
-                                        setValue('data_fim', '');
-                                        setIsSearched(false);
-                                    }}
-                                    hasError={errors.data_inicio || errors.data_fim}
-                                />
-                            </Grid>
-                        )}
-
-                        <Grid size={6}>
-                            <TableFilterButtons
-                                isLoading={isLoadingList}
-                                handleClearFilters={() => {
-                                    setSearchParams((state) => {
-                                        state.delete('name')
-                                        state.delete('page', '1')
-                                        return state
-                                    })
-
-                                    reset({
-                                        communityId: communityId ?? '',
-                                        name: ''
-                                    })
-                                }}
-                            />
-                        </Grid>
+        <Grid container size={12} spacing={2} marginBottom={3} alignItems={'flex-start'} justifyContent="space-between">
+            <Grid item size={8} container spacing={2}>
+                {hasCommunity && (
+                    <Grid item size={4}>
+                        <Controller
+                            name="communityId"
+                            control={control}
+                            defaultValue={communityId}
+                            render={({ field }) => (
+                                <TextField
+                                    select
+                                    label="Comunidade"
+                                    fullWidth
+                                    size="small"
+                                    {...field}
+                                >
+                                </TextField>
+                            )}
+                        />
                     </Grid>
-                </Card>
-            </Collapse>
+                )}
+
+                <Grid item size={4}>
+                    <Controller
+                        name="name"
+                        control={control}
+                        render={({ field }) => (
+                            <TextField
+                                label={"Nome " + itemName}
+                                fullWidth
+                                size="small"
+                                {...field}
+                            />
+                        )}
+                    />
+                </Grid>
+
+                {hasDate && (
+                    <Grid item xs={12} md={4}>
+                        <DateRangePicker
+                            value={[
+                                dateInicio ? dayjs(dateInicio).format("YYYY-MM-DD") : null,
+                                dateFim ? dayjs(dateFim).format("YYYY-MM-DD") : null,
+                            ]}
+                            onChange={(value) => {
+                                const [start, end] = value;
+                                setValue("data_inicio", start ? dayjs(start).format("YYYY-MM-DD") : "");
+                                setValue("data_fim", end ? dayjs(end).format("YYYY-MM-DD") : "");
+                            }}
+                            onReset={() => {
+                                setValue("data_inicio", "");
+                                setValue("data_fim", "");
+                            }}
+                        />
+                    </Grid>
+                )}
+            </Grid>
+
+            <Grid item size={4}>
+                <TableFilterButtons
+                    isLoading={isLoading}
+                    handleClearFilters={() => {
+                        setSearchParams((state) => {
+                            state.delete('communityId');
+                            state.delete('nome');
+                            state.delete('data_inicio');
+                            state.delete('data_fim');
+                            state.set('page', '1')
+                            return state;
+                        });
+
+                        reset({
+                            communityId: '',
+                            nome: '',
+                            data_inicio: '',
+                            data_fim: '',
+                        });
+                    }}
+                />
+            </Grid>
         </Grid>
     );
 }
+
